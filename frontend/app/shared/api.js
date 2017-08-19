dc17dcc0370da81935848b1499f8f683a767fd48
Schnitzel <michael@amazee.com>
2017-08-19 08:14:23 -0400

    running frontend in dev

--- frontend/app/shared/api.js
+++ frontend/app/shared/api.js
@@ -1,7 +1,7 @@
 // @flow
 
-export const apiVersion = '654faa826ec46253dcd4c083ecd32b8db64e2e98';
+export const apiVersion = '58d3eb23a698931d00db2ab19f22369a0ab6d32e';
 
 export {
   default as queryMap,
-} from 'queries/654faa826ec46253dcd4c083ecd32b8db64e2e98.json';
+} from 'queries/58d3eb23a698931d00db2ab19f22369a0ab6d32e.json';

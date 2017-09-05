4077566262495701cc1d86d87282bed95f17a43d
Schnitzel <michael@amazee.com>
2017-09-05 18:05:39 +0200

    hello

--- frontend/app/screens/Home/component.js
+++ frontend/app/screens/Home/component.js
@@ -8,7 +8,7 @@ import styles from './styles.css';
 const Home = (): React.Element<any> =>
   (<div className={styles.Wrapper}>
     <Helmet title="Welcome" />
-    <h1>Welcome!</h1>
+    <h1>Welcome</h1>
     <p>
       This is a simple demo application for Decoupled Drupal using React and
       GraphQL.
